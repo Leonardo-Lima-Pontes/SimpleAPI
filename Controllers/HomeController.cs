55a42aeee4ac0151ee9dcfd89fe389f06503790d
Leonardo Lima Pontes <87279897+Leonardo-Lima-Pontes@users.noreply.github.com>
2022-01-23 23:17:11 -0300

    metodo de put e delete adicionado ao todo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,17 +8,49 @@ namespace ToDoApp.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        [Route("/listastodos/")]
+        [Route("/listtodos/")]
         public IEnumerable<Todo> Get([FromServices] Context context)
         => context.Todos.ToList();
 
+        [HttpGet]
+        [Route("/listtodo/{id:int}")]
+        public Todo GetById([FromRoute] int id, [FromServices] Context context)
+        => context.Todos.FirstOrDefault(x => x.Id == id);
+
         [HttpPost]
-        [Route("/gravartodo/")]
+        [Route("/savetodo/")]
         public Todo Post([FromBody] Todo todo, [FromServices] Context context)
         {
             context.Todos.Add(todo);
             context.SaveChanges();
             return todo;
+        }
+
+        [HttpPut]
+        [Route("/updattodo/{id:int}")]
+        public string Put([FromRoute] int id, [FromBody] Todo todo, [FromServices] Context context)
+        {
+            var oldTodo = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (oldTodo is null) return "Todo not found";
+
+            oldTodo.Name = todo.Name;
+            oldTodo.Done = todo.Done;
+            oldTodo.CreatedAt = todo.CreatedAt;
+
+            context.Todos.Update(oldTodo);
+            context.SaveChanges();
+            return "The todo has been updated successufuly";
+        }
+
+        [HttpDelete]
+        [Route("/deletetodo/{id:int}")]
+        public string Delete([FromRoute] int id, [FromServices] Context context)
+        {
+            var todo = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todo is null) return "Todo not found";
+            context.Todos.Remove(todo);
+            context.SaveChanges();
+            return "Todo deleted successufuly";
         }
     }
 }
